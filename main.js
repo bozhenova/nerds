9979e8538357cfdc7f53dfbd8078c946fd26d727
Margo <margo.bozhenova@gmail.com>
2019-10-10 03:33:33 +0300

    Finished footer, corected positions

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ ymaps.ready(init);
 
 function init() {
   var map = new ymaps.Map("map", {
-    center: [59.93876565029051, 30.322475142852795],
+    center: [59.938954, 30.322061],
     zoom: 17,
     controls: ["zoomControl"],
     behaviors: ["drag"]
   });
 
   var placemark = new ymaps.Placemark(
-    [59.93876565029051, 30.322475142852795],
+    [59.938631, 30.323055],
     {},
 
     {
       iconLayout: "default#image",
       iconImageHref: "img/map-marker.png",
-      iconImageSize: [367, 190]
+      iconImageSize: [231, 190],
+      iconImageOffset: [-45, -195]
     }
   );
 
